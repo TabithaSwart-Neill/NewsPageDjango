8417cab059152db8ffff8fffb733d5ad0c18e9d2
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-08-25 20:33:38 +1000

    Zoom 25 Aug END Sessions
    until step 11 ( the end of session )

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -22,8 +22,12 @@ class StoryView(generic.DetailView):
     template_name = 'news/story.html' 
     context_object_name = 'story'
 
-class AddStoryView(generic.CreateView): 
-    form_class = StoryForm 
-    context_object_name = 'storyForm' 
-    template_name = 'news/createStory.html' 
+class AddStoryView(generic.CreateView):
+    form_class = StoryForm
+    context_object_name = 'storyForm'
+    template_name = 'news/createStory.html'
     success_url = reverse_lazy('news:index')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
 
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls'))
 ]
 
