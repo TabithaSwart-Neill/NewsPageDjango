cdf4eff73150b87f1a6d0deffc9ec0eaaa023c0d
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-08-25 19:19:11 +1000

    Zoom 25 Aug till step 7

--- she_codes_news/she_codes_news/urls.py
+++ she_codes_news/she_codes_news/urls.py
@@ -20,4 +20,8 @@ urlpatterns = [
 
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('users/', include('django.contrib.auth.urls'))
 ]
+
+# http://localhost:8000/users/login/
+# http://localhost:8000/users/logout/
