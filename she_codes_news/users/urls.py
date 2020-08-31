8f3713c7a48537f34d67d256477538faed04188b
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-08-31 12:25:29 +1000

    NewsProject
    Part1 "Order by date" and "Style the form for adding new stories - background colour"
    
    Part2 "Account view so authors can see their profile information"

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from .views import CreateAccountView
+from .views import ChangeAccountView
+from .views import AccountView
 
 app_name = 'users'
 
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(),
-name='createAccount'), 
+name='createAccount'),
+    path('change-account/', ChangeAccountView.as_view(),
+name='changeAccount'),  
+    path('view-account/<int:pk>', AccountView.as_view(),
+name='viewAccount'),
 ]
